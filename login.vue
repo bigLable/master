36b8ee450304d4e0e9e077c47f9e986f7cf5ffbb
yang <1216102533@qq.com>
2020-03-02 22:05:46 +0800

    编辑登录

--- login.vue
+++ login.vue
@@ -1,12 +1,12 @@
 <!--
  * @Author: your name
  * @Date: 2020-03-02 21:32:57
- * @LastEditTime: 2020-03-02 21:36:23
+ * @LastEditTime: 2020-03-02 21:55:25
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \gitgub-workFlow\login.vue
  -->
 
 <template>
-    <span>feature-新增登录功能</span>
+    <span>feature-新增登录功能-登陆毛病fix。。</span>
 </template>
