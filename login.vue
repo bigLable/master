3cef6c69a77996c24466a1b751d91798be8ed979
yang <1216102533@qq.com>
2020-03-02 22:26:54 +0800

    注册修复

--- login.vue
+++ login.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-03-02 21:32:57
- * @LastEditTime: 2020-03-02 22:13:55
+ * @LastEditTime: 2020-03-02 22:26:26
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \gitgub-workFlow\login.vue
@@ -10,6 +10,6 @@
 <template>
 <div>
     <span>feature-新增登录功能-登陆毛病fix。。</span>
-    <p>注册功能</p>
+    <p>注册功能-修复完成</p>
 </div>
 </template>
